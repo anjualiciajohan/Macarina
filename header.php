9058d25470d72a71c7805da549e12273e83e010a
Luqman Hakim <57467912+Luqman2107@users.noreply.github.com>
2019-12-29 18:48:20 +0700

    header

--- header.php
+++ header.php
@@ -81,23 +81,10 @@ else {
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
 	          
-	          <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
-
-	          <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
-              <div class="dropdown-menu" aria-labelledby="dropdown04">
-              	<a class="dropdown-item" href="shop2.php">Shop</a>
-              	<a class="dropdown-item" href="singleproduct.php">Single Product</a>
-                <a class="dropdown-item" href="checkout.html">Checkout</a>
-              </div>
-            </li>
-	          	<li class="nav-item dropdown">
-              		<a class="nav-link dropdown-toggle" href="about.php" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tentang</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown04">
-              	<a class="dropdown-item" href="profilperusahaan.php">Profil Perusahaan</a>
-              	<a class="dropdown-item" href="prosesproduksi.php">Proses Produksi</a>
-              </div>
-            </li>
+	          <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+			  <li class="nav-item"><a href="shop2.php" class="nav-link">Shop</a></li>
+	          <li class="nav-item"><a href="profilperusahaan.php" class="nav-link">Tentang</a></li>
+	          	
 			<li class="nav-item"><a href="testi.php" class="nav-link">Testi</a></li>
 	          <li class="nav-item"><a href="contact.php" class="nav-link">Kontak</a></li>
 	          <li class="nav-item cta cta-colored"><a href="cart.php" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
