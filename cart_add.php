5341c93a569d5d5b1dbdddaaff0eba4cc05df309
hammer021 <55732359+hammer021@users.noreply.github.com>
2020-01-02 01:40:59 +0700

    Update cart_add.php

--- cart_add.php
+++ cart_add.php
@@ -4,9 +4,9 @@
     session_start();
     $item_id=$_GET['id'];
     $harga=$_GET['price'];
-   
+    $qty=1;
     $user_id=$_SESSION['id'];
-    $add_to_cart_query="insert into detail_transaksi(kd_barang,id_reseller,subtotal,status) values ('$item_id','$user_id','$harga','Added to cart')";
-    $add_to_cart_result=mysqli_query($koneksi,$add_to_cart_query) or die(mysqli_error($con));
+    $add_to_cart_query="insert into detail_transaksi(kd_barang,id_reseller,qty_det,subtotal,status) values ('$item_id','$user_id','$qty','$harga','Added to cart')";
+    $add_to_cart_result=mysqli_query($koneksi,$add_to_cart_query) or die(mysqli_error($koneksi));
     header('location: cart.php');
 ?>
