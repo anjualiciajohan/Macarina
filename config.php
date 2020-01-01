babe377ac805dca13dda549608f2560517ee6589
Luqman Hakim <e41180965@student.polije.ac.id>
2020-01-01 15:01:07 +0700

    delete

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost","root","","macarina");
-
-if(!$koneksi){
-    echo "error connection";
-}
-
-?>
--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $hostname  = "localhost";
-   $username  = "root";
-   $password  = "";
-   $dbname  = "macarina";
-   $db = new mysqli($hostname, $username, $password, $dbname);
-?>
