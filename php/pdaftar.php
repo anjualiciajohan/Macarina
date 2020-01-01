a3a038305504324de8fe096367eb7411b32632f6
hammer021 <55732359+hammer021@users.noreply.github.com>
2020-01-01 20:47:15 +0700

    backend daftar
    fixing function daftar

--- php/pdaftar.php
+++ php/pdaftar.php
@@ -4,18 +4,17 @@
    $nama = $_POST['nama-lengkap'];
    $alamat = $_POST['alamat'];
    $no_tlp = $_POST['no-hp'];
-   $fileName = $_FILES['scan_ktp']['
-   name'];
+   $fileName = $_FILES['scan_ktp']['name'];
    $fileName2 = $_FILES['foto']['name'];
    $no_ktp = $_POST['no-ktp'];
    $email = $_POST['email'];
    $pass = $_POST['psw'];
    $rptpass = $_POST['psw-repeat'];
-   $status = "nonaktif";
+   $status = 0;
 
    if($pass == $rptpass){
 
-   mysqli_query($koneksi, "insert into reseller (id_reseller, nama_reseller, alamat, no_tlp, scan_ktp, no_ktp, email, password, status, pas_foto) VALUES ('','$nama','$alamat ','$no_tlp','$fileName','$no_ktp ','$email  ','$pass  ','$status ','$fileName2')");
+   mysqli_query($koneksi, "insert into reseller (nama_reseller, alamat, no_tlp, scan_ktp, no_ktp, email, password, status, pas_foto) VALUES ('$nama','$alamat ','$no_tlp','$fileName','$no_ktp ','$email  ','$pass  ','$status ','$fileName2')");
     move_uploaded_file($_FILES['scan_ktp']['tmp_name'], "../admin/img/reseller/".$_FILES['scan_ktp']['name']);
     move_uploaded_file($_FILES['foto']['tmp_name'], "../admin/img/reseller/foto/".$_FILES['foto']['name']);
     header("location:../index.php");
