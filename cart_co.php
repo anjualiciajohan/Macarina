d00449957a2c4a18d78b0fc633c5b0f73ee00bb5
hammer021 <55732359+hammer021@users.noreply.github.com>
2020-01-02 02:08:18 +0700

    Update cart_co.php

--- cart_co.php
+++ cart_co.php
@@ -5,14 +5,17 @@
     //$item_id=$_GET['id'];
     $user_id=$_SESSION['id'];
     $tgl=date('Y-m-d');
-    $grand = $_GET['grand'];
+    $grand = $_GET['tot'];
+    $kab =$_GET['kabupaten'];
+    $kec =$_GET['kecamatan'];
+    $kel =$_GET['kelurahan'];
     $add_to_cart_query="UPDATE detail_transaksi SET status = REPLACE(status, 'Added to cart', 'Pending') WHERE id_reseller = '$user_id';";
     $add_to_cart_result=mysqli_query($koneksi,$add_to_cart_query) or die(mysqli_error($koneksi));
 
     $add_to_trans ="insert into transaksi(tgl_transaksi,grand_total,id_reseller) values ('$tgl','$grand','$user_id')";
     $add_to_trans_result=mysqli_query($koneksi,$add_to_trans) or die(mysqli_error($koneksi));
 
-    $insert_alamat ="insert into alamat_kirim(id_reseller,kd_kab,sys_code,kd_kel) values ('$user_id','$grand','$user_id')";
+    $insert_alamat ="insert into alamat_kirim(id_reseller,kd_kab,sys_code,kd_kel) values ('$user_id','$kab','$kec','$kel')";
     $alamat_result=mysqli_query($koneksi,$insert_alamat) or die(mysqli_error($koneksi));
     header('location: checkout.php');
 ?>
