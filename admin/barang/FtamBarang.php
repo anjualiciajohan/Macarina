8491631d6a1741db8915a3246a46da9efb07900a
hammer021 <55732359+hammer021@users.noreply.github.com>
2020-01-01 21:16:20 +0700

    backend tambah barang

---
+++ admin/barang/FtamBarang.php
@@ -0,0 +1,23 @@
+<?php
+// include database connection 
+include '../config/config.php';
+
+if (isset($_POST['btn_simpan'])){
+    $fileName = $_FILES['gambar']['name'];
+    $id = $_POST['txt_idbrg'];
+
+    $nama_brg=$_POST['txt_nama_brg'];
+    $harga=$_POST['txt_harga'];
+    $stok=$_POST['txt_stok'];
+    $desc=$_POST['txt_desc'];
+    // update data 
+  
+    mysqli_query($koneksi, "insert into barang (kd_barang, nama_barang, harga, stok, gambar_brg,deskripsi) VALUES ('$id','$nama_brg','$harga','$stok','$fileName','$desc')");
+    move_uploaded_file($_FILES['gambar']['tmp_name'], "../img/barang/".$_FILES['gambar']['name']);
+    echo"<script>alert('Gambar Berhasil diupload !');history.go(-1);</script>";
+    header("location:tBarang.php");
+}
+
+?>
+
+
