30ab05366683dde4a41cf303f0dd6bd44bd40ce0
KurniaJuanSyahM <KurniaJuanSyahM@github.com>
2020-01-01 19:35:28 +0700

    tentang.php

---
+++ tentang.php
@@ -0,0 +1,69 @@
+<?php 
+include_once "header.php";
+?>
+
+    <div class="hero-wrap hero-bread" style="background-image: url('images/edit.jpg');">
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+          	<p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home</a></span></p>
+            <h1 class="mb-0 bread">About</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="ftco-section ftco-no-pb ftco-no-pt bg-light">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-5 p-md-5 img img-2 d-flex justify-content-center align-items-center">
+          <iframe width="420" height="315"src="https://youtube.com/embed/7l00kYjjNe4?autoplay=1"></iframe>
+					</div>
+          </div>
+          </div>
+					<div class="col-md-7 py-5 wrap-about pb-md-5 ftco-animate">
+	          <div class="heading-section-bold mb-4 mt-md-5">
+	          	<div class="ml-md-0">
+	            </div>
+	          </div>
+	          <div class="pb-md-5">
+							<p>Nama Macarina yang dipakai tersebut didapat dari singkatan "Makaroni Nagih",karena pada saat mencobakan macaroni pertama kali kepada orang-orang di sekitar,
+              mereka berpendapat bahwa makanan ringannya tersebut membuat mereka merasa nagih atau ketagihan untuk mencicipi terus menerus. oleh karena itu empat orang yaitu
+              Sdr. Helmi dan rekannya sepakat untuk menggunakan nama"Macarina" sebagai nama pada brand produk makanan ringan mereka tersebut. </p><br/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+    	
+   
+
+<?php 
+include_once "footer.php";
+?>
+
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
