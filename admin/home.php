58f536fca90d84a6e416e983f88ffa31bda7586e
Luqman Hakim <57467912+Luqman2107@users.noreply.github.com>
2020-01-01 18:36:32 +0700

    home.php

--- admin/home.php
+++ admin/home.php
@@ -204,46 +204,7 @@ include_once "topNavbar.php";
                   </div>
                 </div>
               </div>
-            </div>
-
-            <!-- Pie Chart -->
-            <div class="col-xl-4 col-lg-5">
-              <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Penjualan</h6>
-                  <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                      <div class="dropdown-header">Dropdown Header:</div>
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                  </div>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                  <div class="chart-pie pt-4 pb-2">
-                    <canvas id="myPieChart"></canvas>
-                  </div>
-                  <div class="mt-4 text-center small">
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-primary"></i> Pack
-                    </span>
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-success"></i> Box
-                    </span>
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-info"></i> Paket
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            </div>            
           </div>
                 
 
@@ -276,10 +237,6 @@ include_once "topNavbar.php";
                     <div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
-              </div>
-
-              <!-- Color System -->
-             
               </div>
 
             </div>
