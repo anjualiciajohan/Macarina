7f18452786715fb2ec16984b096e8027ee9a5eaa
Luqman Hakim <e41180965@student.polije.ac.id>
2020-01-01 21:37:08 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,5 @@
 <?php
    session_start();
    session_destroy();
+   header('location: index.php');
 ?>
-
-<div align="center">
-  <h2>Anda telah berhasil logout..</h2>
-  Silahkan klik <a href="login.php">disini</a> untuk login kembali
-</div>
