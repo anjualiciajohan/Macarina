1dc344efe698da1048abed3f1d9e6ebb4c2c7e33
baidowii <baidowi.jr.10@gmail.com>
2020-01-01 20:22:37 +0700

    Create contact.php
    memberikan gmaps

---
+++ contact.php
@@ -0,0 +1,161 @@
+<?php 
+include_once "header.php";
+?>
+ <!-- breadcrumb start-->
+    <div class="hero-wrap hero-bread" style="background-image: url('images/bgcontact.jpg');">
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+          	<p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home</a></span></p>
+            <h1 class="mb-0 bread">Contact Us</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+    <div class="row d-flex contact-info mb-5">
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Outlet 1</h3>
+              <p>Jl. Kalimantan No.39B, Krajan Timur, Sumbersari</p>
+              <a target="_blank" rel="nofollow"  href="https://g.page/Macarina-Kalimantan?share" class="smoothscroll btn btn-primary">Maps</a></p>
+	            <p>
+	          </div>
+          </div>
+         <div class="row d-flex contact-info mb-5">
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Outlet 2</h3>
+              <p>Jl. Mastrip,</p>
+              <a target="_blank" rel="nofollow"  href="https://goo.gl/maps/K2VvcB5CapDRJN4QA" class="smoothscroll btn btn-primary">Maps</a></p>
+	            <p> 
+	          </div>
+          </div>
+          </div>
+          </div>
+         
+
+   <!-- breadcrumb start-->
+
+    <!-- ================ contact section start ================= -->
+     
+
+        <section class="ftco-section contact-section ftco-no-pb" id="contact-section">
+      <div class="container align-items-center justify-content-center">
+      	
+
+        <div class="row d-flex contact-info mb-5">
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Alamat</h3>
+              <p>
+              Jl. Sriwijaya XX No.11 Karangrejo - Sumbersari, JEMBER</p>
+              <a target="_blank" rel="nofollow"  href="https://g.page/Macarina?share" class="smoothscroll btn btn-primary">Lihat di Gmaps</a></p>
+	            <p>
+	          </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-phone2"></span>
+          		</div>
+          		<h3 class="mb-4">Kontak Langsung</h3>
+	            <p><a >+ 6281 217 757 777</a></p>
+              <a target="_blank" rel="nofollow"  href="https://api.whatsapp.com/send?phone=628887116773&text=Kak,%20Saya%20dari%20website%20,mau%20pesan%20Macarina:" class="smoothscroll btn btn-primary">Pesan via Whatsapp</a></p>
+	            <p>
+	          </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-paper-plane"></span>
+          		</div>
+          		<h3 class="mb-4">Alamat Email</h3>
+	            <p><a href="mailto:macarina.official@gmail.com">macarina.idl@gmail.com</a></p>
+	          </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-globe"></span>
+          		</div>
+          		<h3 class="mb-4">Website</h3>
+	            <p><a href="#">macarinaid.com</a></p>
+	          </div>
+          </div>
+        </div>
+
+        <div class="row no-gutters block-9">
+          <div class="col-md-6 order-md-last d-flex">
+            <form action="#" class="bg-light p-4 p-md-5 contact-form">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Your Name">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Your Email">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Subject">
+              </div>
+              <div class="form-group">
+                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+              </div>
+              <div class="form-group">
+                <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
+              </div>
+            
+            </form>
+          
+          </div>
+
+          <div class="col-md-6 d-flex">
+          	<div class="img" style="background-image: url(images/macarinashake.jpg);"></div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </section>
+		
+    <!-- ================ contact section end ================= -->
+
+   
+
+    <?php 
+include_once "footer.php";
+?>
+
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
